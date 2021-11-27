87124a4dbe5d7efbdc9fc18f2d141445ee8184ed
kazukiyokoo <67489015+kaz1027@users.noreply.github.com>
2021-11-27 16:33:31 +0900

    コミット

--- js/script.js
+++ js/script.js
@@ -85,6 +85,36 @@ $('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
 		$(this).removeClass('fadeLeft');// 画面外に出たらfadeLeftというクラス名を外す
 		}
 		});
+	$('.fadeLeftTrigger').each(function(){ //fadeLeftTriggerというクラス名が
+		var elemPos = $(this).offset().top-50;//要素より、50px上の
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight){
+		$(this).addClass('fadeLeft');// 画面内に入ったらfadeLeftというクラス名を追記
+		}else{
+		$(this).removeClass('fadeLeft');// 画面外に出たらfadeLeftというクラス名を外す
+		}
+		});
+	$('.fadeLeftTrigger1').each(function(){ //fadeLeftTriggerというクラス名が
+		var elemPos = $(this).offset().top-50;//要素より、50px上の
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight){
+		$(this).addClass('fadeLeft1');// 画面内に入ったらfadeLeftというクラス名を追記
+		}else{
+		$(this).removeClass('fadeLeft1');// 画面外に出たらfadeLeftというクラス名を外す
+		}
+		});
+	$('.fadeLeftTrigger2').each(function(){ //fadeLeftTriggerというクラス名が
+		var elemPos = $(this).offset().top-50;//要素より、50px上の
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight){
+		$(this).addClass('fadeLeft2');// 画面内に入ったらfadeLeftというクラス名を追記
+		}else{
+		$(this).removeClass('fadeLeft2');// 画面外に出たらfadeLeftというクラス名を外す
+		}
+		});
     //4-1 ふわっ（右から）
 	$('.fadeRightTrigger').each(function(){ //fadeRightTriggerというクラス名が
 		var elemPos = $(this).offset().top-50;//要素より、50px上の
@@ -96,19 +126,40 @@ $('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
 		$(this).removeClass('fadeRight');// 画面外に出たらfadeRightというクラス名を外す
 		}
 		});
+	$('.fadeRightTrigger1').each(function(){ //fadeRightTriggerというクラス名が
+		var elemPos = $(this).offset().top-50;//要素より、50px上の
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight){
+		$(this).addClass('fadeRight1');// 画面内に入ったらfadeRightというクラス名を追記
+		}else{
+		$(this).removeClass('fadeRight1');// 画面外に出たらfadeRightというクラス名を外す
+		}
+		});
+	$('.fadeRightTrigger2').each(function(){ //fadeRightTriggerというクラス名が
+		var elemPos = $(this).offset().top-50;//要素より、50px上の
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight){
+		$(this).addClass('fadeRight2');// 画面内に入ったらfadeRightというクラス名を追記
+		}else{
+		$(this).removeClass('fadeRight2');// 画面外に出たらfadeRightというクラス名を外す
+		}
+		});
+
+
+		$('.blurTrigger').each(function(){ //blurTriggerというクラス名が
+			var elemPos = $(this).offset().top-50;//要素より、50px上の
+			var scroll = $(window).scrollTop();
+			var windowHeight = $(window).height();
+			if (scroll >= elemPos - windowHeight){
+			$(this).addClass('blur');// 画面内に入ったらblurというクラス名を追記
+			}else{
+			$(this).removeClass('blur');// 画面外に出たらblurというクラス名を外す
+			}
+			}); 
 }
 
-$('.blurTrigger').each(function(){ //blurTriggerというクラス名が
-    var elemPos = $(this).offset().top-50;//要素より、50px上の
-    var scroll = $(window).scrollTop();
-    var windowHeight = $(window).height();
-    if (scroll >= elemPos - windowHeight){
-    $(this).addClass('blur');// 画面内に入ったらblurというクラス名を追記
-    }else{
-    $(this).removeClass('blur');// 画面外に出たらblurというクラス名を外す
-    }
-    }); 
-  
 
 
 // 画面をスクロールをしたら動かしたい場合の記述
